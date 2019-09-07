5630c076438caa6f38bdacc65644653cf948230a
sukhmeetsd <sukhmeetsd@gmail.com>
2019-09-07 18:30:36 -0400

    Made to compile

--- Supervisor/proj1/lib/sequence/localserver.ex
+++ Supervisor/proj1/lib/sequence/localserver.ex
@@ -1,20 +1,9 @@
-# ---
-# Excerpted from "Programming Elixir",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/elixir for more book information.
-# ---
 defmodule Local.Server do
   use GenServer
 
   def init(init_arg) do
     {:ok, init_arg}
   end
-
-  #####
-  # External API  
 
   def start_link(start_n, end_n) do
     # get_vamp(start_n, end_n)
@@ -26,67 +15,20 @@ defmodule Local.Server do
     GenServer.call(__MODULE__, {:vamper, start_n, end_n})
   end
 
-  # def increment_number(delta) do
-  #   GenServer.cast(__MODULE__, {:increment_number, delta})
-  # end
-
-  #####
-  # GenServer implementation
-
   def handle_call({:vamper, start_n, end_n}, _from, state) do
     # [head | tail] = state
     prn_vamps(start_n, end_n)
     {:reply, state, state}
   end
 
-  # def handle_cast({:increment_number, delta}, current_number) do
-  #   {:noreply, current_number + delta}
-  # end
-
-  # def format_status(_reason, [_pdict, state]) do
-  #   [data: [{'State', "My current state is '#{inspect(state)}', and I'm happy"}]]
-  # end
-
   def prn_vamps(arg_n, arg_k) do
-    # {:ok, pid} = Mix.Tasks.PerfectSquareComputer.start_link() 
-    # IO.puts "N=#{arg_n} K=#{arg_k}"                       #Start core logic module
-    # Logger.info "Just before spawning"
-
-    # (arg_n..arg_k)
-    # |> Enum.map(fn(each_n) 
-    #   -> 
-    #     GenServer.cast(pid, {:check_vamp, each_n}) 
-    #   end
-    #   )
-    #   res  = GenServer.call(pid, {:ash},5000)
-    #   IO.inspect res
-    # al = Process.alive?(pid)
-    # IO.puts "alive: #{al}"
-
-    # res  = GenServer.call(pid, {:ash},50000)
-    # IO.inspect res
-    # res = vampire_factors(125460)
-    # IO.inspect res
     arg_n..arg_k
     |> Enum.map(fn each_n ->
       _pid = spawn(__MODULE__, :isVampire, [each_n])
-      # Process.monitor(pid)
-      # receive do
-      #   msg -> msg
-      # end
     end)
-
-    # arg_n..arg_k 
-    # |> Task.async_stream(&Mix.Tasks.Boss.vamp_check/1, max_concurrency: System.schedulers_online) 
-    # |> Enum.map(fn {:ok, _result} -> nil end)
-
-    # IO.puts ""
-    # (1..arg_n)
-    # |> Enum.map(fn(each_n) -> spawn(__MODULE__, :work, [pid, each_n, arg_k]) end)    #spawn multiple process running in parallel
   end
 
   def isVampire(n) do
-    # IO.puts "Working on #{n} in vamp_check"
     case getFangs(n) do
       [] ->
         nil
@@ -100,10 +42,6 @@ defmodule Local.Server do
         # {:os.system_time(:millisecond)}
     end
   end
-
-  # def work(pid, each_n, arg_k) do
-  #   GenServer.call(pid, {:ash, each_n, arg_k})                              #call core logic to compute solution
-  # end
 
   def getFangs(n) do
     if Integer.mod(char_len(n), 2) == 1 do
